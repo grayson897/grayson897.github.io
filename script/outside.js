9a091d462a8593210ea77850f87255cc3773099b
grayson897 <114687055+grayson897@users.noreply.github.com>
2023-02-18 21:51:42 -0600

    more testing

--- script/outside.js
+++ script/outside.js
@@ -96,27 +96,27 @@ var Outside = {
 	},
 	TrapDrops: [
 		{
-			rollUnder: 100,
+			rollUnder: 0.5,
 			name: 'fur',
 			message: _('scraps of fur')
 		},
 		{
-			rollUnder: 100,
+			rollUnder: 0.75,
 			name: 'meat',
 			message: _('bits of meat')
 		},
 		{
-			rollUnder: 100,
+			rollUnder: 0.85,
 			name: 'scales',
 			message: _('strange scales')
 		},
 		{
-			rollUnder: 100,
+			rollUnder: 0.93,
 			name: 'teeth',
 			message: _('scattered teeth')
 		},
 		{
-			rollUnder: 100,
+			rollUnder: 0.995,
 			name: 'cloth',
 			message: _('tattered cloth')
 		},
